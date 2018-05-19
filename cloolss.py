45118ee2f3dba991e7fd0a2c0eb4a76bd1112949
Jennifer Taggart <jennifer.taggart.work@gmail.com>
2018-05-19 06:48:56 -0700

    Wrote match function for outfit colors

--- cloolss.py
+++ cloolss.py
@@ -1,11 +1,33 @@
 # Run me with the Python interpreter for now
 # I am CLothing Organizer/Optimizer Logical Style System!
-print("Hello World, I'm CLOOLSS!")
-print("CLothing Organizer/Optimizer Logical Style System!")
-# Do LOTS of checking to make sure files are 
 import glob
 files = glob.glob('./*.txt')
+config = open('cloolss.cfg').read()
 
+# Do LOTS of checking to make sure files are formatted correctly
+#def checkfiles ()
+
+# Tests whether a new clothing item matches any items in outfit by looping over all
+# colors in outfit and checking them against all colors in the new clothing item
+# returns match if any two colors match
+def match(outfit, clothingitem):
+	outfitcolors = outfit[1]
+	clothingitemcolors = clothingitem[1]
+	for color in outfitcolors:
+		print(color)
+		for anothercolor in clothingitemcolors:
+			print(anothercolor)
+			if (color == anothercolor):
+				print("Match!")
+				return match 				
+
+#Define outfit and clothing item for testing
+outfit = ['scarf', ['purple'], 'loose fitting', 'NA', 'gauze', 'seasonless', 'clean']
+clothingitem = ['scarf', ['yellow', 'green', 'dark green', 'brown', 'pink', 'baby blue', 'orange', 'black'], 'loose fitting', 'NA', 'gauze', 'seasonless', 'clean']
+match(outfit, clothingitem)
+
+print("Hello World, I'm CLOOLSS!")
+print("CLothing Organizer/Optimizer Logical Style System!")
 #Arrays are called lists in python whyyyyy
 # clothingdatabase is going to be the working database of items of clean clothing appropriate to season
 # setting season to dry right now
