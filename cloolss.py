d435464060af381fd79cfd2e39ab728e91a2397d
Jennifer Taggart <jennifer.taggart.work@gmail.com>
2018-05-28 07:37:40 -0700

    Added basic input validation for most lines. Still working on regex stuff for line2 the colors list

--- cloolss.py
+++ cloolss.py
@@ -7,16 +7,70 @@ import random
 import glob
 import re
 
-# Do LOTS of checking to make sure files are formatted correctly
-#def checkfiles ()
+def error_stuff():
+    """ Print error message and exit program
+    """
+    error_message = "Invalid clothing item. Please move this clothing item's file out of the clothing directory!"
+    print(error_message)
+    print(clothing_item)
+    exit()
 
-def clean_and_in_season(clothingitem):
+def validate_input():
+    """ Test the clothing files for valid input. Line 1 must match something 
+    in valid clothing item array. Line 4 must be a number, or NA, or N/A. Line 5 is not checked because
+    we aren't doing anything with it yet. Other lines must match set of 2 or 3 specific sctrings, or NA or N/A
+    """
+    valid_clothing_items = ['shirt', 'shoes', 'scarf', 'hat', 'dress', 'skirt', 'pants', 'shorts', 'sweater', 'blazer']
+    valid_item_flag = 0
+    for item in valid_clothing_items:
+        if (item == clothing_item[0]):
+            valid_item_flag = 1
+    if (valid_item_flag == 0):
+            print("Problem in first line!")
+            error_stuff()
+    print(clothing_item[1])
+    if ((clothing_item[2] != 'loose-fitting') and (clothing_item[2] != 'close-fitting')):
+        if ((clothing_item[2] != 'NA') and (clothing_item[2] != 'N/A')):
+            print("Problem in 3rd line")
+            error_stuff()
+    # Line 2 is read as a string, so can't just check if type is list.
+    # Check for beging with [', end with '], and an even number of 's
+    if (re.search("^\[", clothing_item[2]) != 'None'):
+        if (re.search("$\]", clothing_item[2]) != 'None'):
+            #all_my_little_quotes = re.search("\[", clothing_item[2])
+            #print(all_my_little_quotes)
+            print("Maybe I have a list!")
+#new_thing_colors_line = re.sub(r'\'|\[|\]|\"', '', new_thing_colors_line)
+ #   if isinstance(check_list,list):
+  #      print("Test list success!")        
+   # if not isinstance(clothing_item[2],str):
+    #    print("I am not a list!")
+     #   error_stuff()  
+    # Can try live in the middle of a function like this?          
+    try:
+        return float(clothing_item[3])
+    except ValueError:
+        if ((clothing_item[3] != 'NA') and (clothing_item[3] != 'N/A')):
+            print("Problem in 4th line")
+            error_stuff()  
+    # Not checking line 5 (fabric) yet because no specific plans.                       
+    if (clothing_item[5] != 'dry season'):
+        if (clothing_item[5] != 'rainy season'):
+            if (clothing_item[5] != 'seasonless'):
+                print("Problem is 6th line!")
+                error_stuff()
+    if ((clothing_item[6] != 'clean') and (clothing_item[6] != 'wash')):
+        print("Problem in 7th line!")
+        error_stuff()
+    print(clothing_item[6])        
+
+def clean_and_in_season(thing):
     """Test whether clothing is clean and in-season."""
     if (clothing_item[6] == "clean"):
     	if (clothing_item[5] == season or clothing_item[5] == "seasonless"):
     		return True
 
-def clothing_type(clothingitem):
+def clothing_type(thing):
     """Test whether clothing item is a top, bottom, layer, or accessory 
     and add it to the appropriate array.
     """	
@@ -57,7 +111,7 @@ def match(thing_collection, new_thing, add_neutrals):
         'khaki', 'navy', 'gray']
     else:
         neutrals = []    
-    # Having a hell of a time passing my arrays for clothing items to this 
+    # Having a heck of a time passing my arrays for clothing items to this 
     # function as arrays. *arg1 doesn't work because there are two arrays I'm
     # passing in, I think. So I am ending up with a string. Using split and 
     # regexp to process now, circle back later for better solution.
@@ -122,40 +176,44 @@ def create_outfit():
         print("Fail! Trying again...")
         create_outfit()
         return False
-                      			
-# Main program, need to make this a function, deal with variable scope issues 
-files = glob.glob('./*.txt')
-#config = open('cloolss.cfg').read()
-print("Hello, I'm CLOOLSS!")
-print("CLothing Organizer/Optimizer Logical Style System!")
-#Arrays are called lists in python whyyyyy
-# clothingdatabase is going to be the working database of items of clean 
-# clothing appropriate to season setting season to dry right now
-season = 'dry season'
-clothing_database = []
-possible_bottoms = []
-possible_tops = []
-possible_layers = []
-possible_accessories = []
-possible_shoes = []
+                      		
+def main():
+    print("Hello, I'm CLOOLSS!")
+    print("CLothing Organizer/Optimizer Logical Style System!")
+    print("I am in pre-beta.")
+    files = glob.glob('./*.txt')
+    # I am probably not supposed to be declaring a whole bunch of global 
+    # variables in my main function. Figure out better way to do this. 
+    global season, clothing_database, possible_bottoms, possible_tops 
+    global possible_layers, possible_accessories, possible_shoes
+    season = 'dry season'
+    clothing_database = []
+    possible_bottoms = []
+    possible_tops = []
+    possible_layers = []
+    possible_accessories = []
+    possible_shoes = []
+    # Shoes and scarves
+    for file in files:
+        # Open the file, read in the lines, and split into an array with one 
+        # line per entry
+        global clothing_item
+        clothing_item = open(file).read().splitlines() 
+        validate_input()
+        if clean_and_in_season(clothing_item):
+    	    #print("Item is clean and in season!")
+    	    clothing_type(clothing_item)
+    	    #print("I found out what type of clothing")
+    print("About to call create outfit")        
+    create_outfit()
+    print("That's all I can do right now, bye!")	
 
-# Shoes and scarves
-for file in files:
-    # TO DO: need to do LOTS of validation of input here!
-    # Open the file, read in the lines, and split into an array with one line
-    # per entry
-    clothing_item = open(file).read().splitlines() 
-    if clean_and_in_season(clothing_item):
-	    #print("Item is clean and in season!")
-	    clothing_type(clothing_item)
-	    #print("I found out what type of clothing")
-
-create_outfit()
-
-print("That's all I can do right now, bye!")	
-
+main()
    
-
-# TO DO: Generate three sample outfits (print them out)
 # TO DO: Implement in js with localstorage for washed/not washed, have a 
 # "did laundry" button that sets everything to washed again
+# Set up your own neutrals
+# Pants length/ heel height checking module
+# Better outfit insulation estimation with clo
+# Shoe rest rule where shoes have to rest x number of days after they are worn
+# Should do some unit testing
